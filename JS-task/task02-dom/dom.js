27c5de63881aab6362710cd538892db3eaa64c54
juli59 <molnarjuli59@mail.com>
2021-04-29 17:11:33 +0200

    changes

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,7 @@
 function changeElements() {}
-
+document.querySelectorAll('.example').forEach(e=>{
+    e.style.backgroundColor='red';
+    e.style.fontStyle='italic'
+})
 
 export { changeElements };
